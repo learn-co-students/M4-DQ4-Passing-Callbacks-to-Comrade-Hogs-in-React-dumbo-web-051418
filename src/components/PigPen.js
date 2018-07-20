84515f56bb402458d9011d7a03c793276fcc4836
Sammy Akharaz <ketrixz@gmail.com>
2018-07-20 09:29:16 -0400

    Done.

--- src/components/PigPen.js
+++ src/components/PigPen.js
@@ -24,12 +24,20 @@ export default class PigPen extends React.Component {
     const newState = {environment: 'docile'}
     this.setState(newState)
   }
+  panickx = () => {
+    const newState = {environment: 'inhospitable'}
+    this.setState(newState)
+  }
 
   alterEnvironment = (vibe) => {
-    if (vibe === "inhospitable")
+    console.log(`vibe is ${vibe}`);
+    if (vibe === "inhospitable"){
       this.audio.play()
-    const newState = {environment: vibe}
-    this.setState(newState)
+      this.relax()
+    }
+    else{
+      this.panickx()
+    }
   }
 
   generateSheeple = () => {
@@ -43,7 +51,7 @@ export default class PigPen extends React.Component {
     return(
       <div id="pig-pen">
         {sheeple}
-        <GalaxySNote7 environment={null} alterEnvironment={null} />
+        <GalaxySNote7 environment={this.state.environment} alterEnvironment={this.alterEnvironment} />
       </div>
     )
   }
